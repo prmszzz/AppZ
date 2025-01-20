b1b330d4cade83c2f11d9c04e0a97b0a98429354
prmszzz <alinkazackirowa@yandex.ru>
2025-01-20 09:21:23 +0300

    first commit

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AppZakirova
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace AppZakirova
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
